78489596b3481094003bd294cbecaabea76a3ee3
Mikhail9090 <Mikhail_Churakov@epam.com>
2017-06-18 20:32:06 +0300

    Initial commit

---
+++ src/test/java/TestClass.java
@@ -0,0 +1,13 @@
+import org.testng.annotations.Test;
+
+/**
+ * Created by yan on 08.06.17.
+ */
+public class TestClass {
+
+    @Test
+    public void test1(){
+        System.out.println("Test 1");
+    }
+
+}
